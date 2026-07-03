6617fff2417ee1d1501f83db1b1d3c182850f2e3
Aryan <ganjrearyan16@gmail.com>
2026-07-03 19:17:46 +0530

    Day 23 - lecture 54 55 completed

---
+++ Video 54/script.js
@@ -0,0 +1,15 @@
+alert("Hello World");
+
+console.log("Code is running....")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast you computer")
+
+if(true){
+    console.log("computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
+
+console.log("Your number is " + a)
---
+++ Video 55/script.js
@@ -0,0 +1,40 @@
+console.log("Hey this is tutorial 55")
+
+let a = 5;
+// a = a + 1
+let b = 6;
+let c = "Aryan";
+// var 55d ="Vairagya"; ye invalid hai number se start nahi ho sakta. not allowed
+
+// console.log( a + b)
+// console.log(typeof a, typeof b, typeof c);
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+// const a1 = 6;
+// a1 = a1 + 1;
+// Not allowed because a1 is constant
+
+let x = "AryanGanjare";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name:"Salman",
+    "job code":2116
+}
+
+console.log(o)
+o.salary = "100crores";
+console.log(o)
+o.salary = "500crores";
+console.log(o)
